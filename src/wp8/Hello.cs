e005c9410f8e57a08bc745c5c65c8b5ed5c40e7f
Sasidharan <madhavanindira@users.noreply.github.com>
2015-10-05 16:07:45 +0530

    Update Hello.cs

--- src/wp8/Hello.cs
+++ src/wp8/Hello.cs
@@ -1,1 +1,18 @@
 
+using WPCordovaClassLib.Cordova;
+using WPCordovaClassLib.Cordova.Commands;
+using WPCordovaClassLib.Cordova.JSON;
+
+namespace Cordova.Extension.Commands
+{
+    public class Hello : BaseCommand
+    {
+        public void greet(string args)
+        {
+            string name = JsonHelper.Deserialize<string>(args);
+            string message = "Hello, " + name;
+            PluginResult result = new PluginResult(PluginResult.Status.OK, message);
+            DispatchCommandResult(result);
+        }
+    }
+}
