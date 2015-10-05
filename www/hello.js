dde664a173c7d5cf7555145f48172d191e5529d6
Sasidharan <madhavanindira@users.noreply.github.com>
2015-10-05 16:09:11 +0530

    Update hello.js

--- www/hello.js
+++ www/hello.js
@@ -1,1 +1,8 @@
 
+/*global cordova, module*/
+
+module.exports = {
+    greet: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
+    }
+};
